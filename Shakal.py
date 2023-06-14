62360190825c848e116091c350bb49ade705f0cb
Shchushch <sabterra@hotmail.com>
2023-06-14 06:52:35 +0300

    Исправил пару ошибок

--- Shakal.py
+++ Shakal.py
@@ -10,7 +10,9 @@ st.title("Шакализатор")
 st.write("### Тут можно зашакалить картинку")
 file = st.file_uploader("Кладёшь картинку сюда")
 plt.rcParams.update({'image.cmap': 'gray'})
-@st.cache
+size =10
+
+@st.cache_data
 def shakalizator(img:pd.DataFrame,shakal:int):
     U, sing_vals, V = np.linalg.svd(img)
     trunc_U = U[:, :shakal]
@@ -21,29 +23,32 @@ def shakalizator(img:pd.DataFrame,shakal:int):
     img2=trunc_U@trunc_sigma@trunc_V
     return img2
 
+
 if file is not None:
     img = plt.imread(file)[:, :, 0]
+    size = img.shape[0]
 
 fig,ax = plt.subplots(1,2,sharex=True,sharey=True)
+fig.tight_layout()
 ax[0].set_title("Исходное Изображение")
-ax[1].set_title("Изображение после шакализации")
-if st.button('Чё тут у нас'):
-    
-    ax[0].imshow(img)
-    
-    #st.pyplot(fig)# plt.imshow(img, cmap='gray')
-    
-    #st.pyplot(fig)# plt.imshow(img, cmap='gray')
-else:
-    st.write("Кажется, тут никакой картинки нет")
-shakal = st.number_input('Введите количество шакалов', min_value=1, max_value=img.shape[0], value=img.shape[0])
+ax[0].grid(False)
+ax[0].axis('off')
 
-if st.button('Запустить шакализатор'):
+
+ax[1].set_title("Изображение после шакализации")
+ax[1].grid(False)
+ax[1].axis('off')
+if st.button('Чё тут у нас?'):
     ax[0].imshow(img)
+    #ax[1].figure(figsize=img.shape())
+    fig
+shakal = st.number_input('Введите количество шакалов', min_value=1, max_value=size, value=size)
+
+if st.button('Шакализовать'):
+    ax[0].imshow(img,)
     ax[1].imshow(shakalizator(img,shakal))#shakalizator(img,shakal)
-    
+    fig
 else:
     st.write("Ещё не зашакалили")
-#plt.show(fig)
-fig
+
 
