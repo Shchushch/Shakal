2a027b27551da00557844d07b1cb0bb55758dc2d
Shchushch <sabterra@hotmail.com>
2023-06-14 08:47:08 +0300

    Добавил слайдер и чутка поменял вывод

--- Shakal.py
+++ Shakal.py
@@ -5,15 +5,32 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import streamlit as st
 import os
+"st.session_state object:" , st.session_state
 
 st.title("Шакализатор")
-st.write("### Тут можно зашакалить картинку")
+"### Тут можно ~~зашакалить~~ сжать картинку"
+info=("Эта программа использует сингулярное разложение, для понижения ранга матрицы до заданного пользователем. "
+    "В результате, мы получаем сжатую (aka зашакаленную) картинку. "
+    "Сразу предупрежу, что такой алгоритм очень аккуратный, так что реально заметный результат можно будет увидеть только если сжимать строку "
+    "до пары процентов от изначального размера картинки в пикселях. "
+    "Также хочу отметить, что алгоритм небыстрый, так что обработка изображения с высоким разрешением занимает достаточно много времени")
+#my_container = st.container()
+# if 'my_container' not in st.session_state:
+#     st.session_state.my_container = my_container
+if st.button("Что это такое",type='secondary'):
+    st.divider()
+    info
+    st.divider()
+
+    
+
 file = st.file_uploader("Кладёшь картинку сюда")
+#pic=st.image()
 plt.rcParams.update({'image.cmap': 'gray'})
 size =10
 
 @st.cache_data
-def shakalizator(img:pd.DataFrame,shakal:int):
+def shakalizator(img:pd.DataFrame,shakal:int)->pd.DataFrame: 
     U, sing_vals, V = np.linalg.svd(img)
     trunc_U = U[:, :shakal]
     trunc_V = V[:shakal, :]
@@ -21,34 +38,41 @@ def shakalizator(img:pd.DataFrame,shakal:int):
     np.fill_diagonal(sigma, sing_vals)
     trunc_sigma = sigma[:shakal, :shakal]
     img2=trunc_U@trunc_sigma@trunc_V
+    #st.write(type(img2))
     return img2
-
+def update_slider():
+    st.session_state.slider = st.session_state.numeric
+def update_numin():
+    st.session_state.numeric = st.session_state.slider 
 
 if file is not None:
     img = plt.imread(file)[:, :, 0]
     size = img.shape[0]
-
-fig,ax = plt.subplots(1,2,sharex=True,sharey=True)
-fig.tight_layout()
-ax[0].set_title("Исходное Изображение")
-ax[0].grid(False)
-ax[0].axis('off')
-
-
-ax[1].set_title("Изображение после шакализации")
-ax[1].grid(False)
-ax[1].axis('off')
-if st.button('Чё тут у нас?'):
-    ax[0].imshow(img)
-    #ax[1].figure(figsize=img.shape())
-    fig
-shakal = st.number_input('Введите количество шакалов', min_value=1, max_value=size, value=size)
-
-if st.button('Шакализовать'):
-    ax[0].imshow(img,)
-    ax[1].imshow(shakalizator(img,shakal))#shakalizator(img,shakal)
-    fig
-else:
-    st.write("Ещё не зашакалили")
+with st.container():
+    col1, col2 = st.columns([1,1])
+with col1:
+    "Изображение до шакализации"
+    if st.button('Чё тут у нас?',type='primary',key='pic_show'):
+        st.image(img)
+shakal = st.number_input('Введите количество шакалов',key = 'numeric', min_value=1, max_value=size, value=size,on_change=update_slider)
+slider_value = st.slider('Можете ещё вот так выбрать', min_value = 1, 
+                        value = shakal, 
+                        max_value = size,
+                        step = 1,
+                        key = 'slider', on_change= update_numin)
+fig,ax = plt.subplots()
+ax.grid(False)
+ax.axis('off')
+with col2:
+    "Изображение до шакализации"
+    if st.button('Шакализовать',type='primary',key='shakal_pic'):
+        shakaled = shakalizator(img,shakal)
+        ax.imshow(shakaled)
+        #st.image(fig)
+        #st.pyplot(ax.imshow(shakaled))
+        fig
+    else:
+        text='Ещё не зашакалили'
+        st.markdown(f"<div style='text-align: center;vertical-align: middle'>{text}</div>",unsafe_allow_html=True)
 
 
